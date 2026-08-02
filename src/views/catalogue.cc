56e106931d111c67495931db856b62e71a2abb8d
David Given <dg@cowlark.com>
2026-08-02 19:40:18 +0200

    Build view catalogue with initializer list

--- src/views/catalogue.cc
+++ src/views/catalogue.cc
@@ -4,26 +4,15 @@
 
 #include <map>
 #include <string>
-#include <utility>
 
 #include "components.h"
 #include "view.h"
 
-namespace
-{
-
-    std::map<std::string, fruit::Component<View> (*)()> BuildViewCatalogue()
-    {
-        std::map<std::string, fruit::Component<View> (*)()> catalogue;
-        catalogue.emplace("HostnameView", GetViewComponent);
-        return catalogue;
-    }
-
-} // namespace
-
 const std::map<std::string, fruit::Component<View> (*)()>& GetViewCatalogue()
 {
     static const std::map<std::string, fruit::Component<View> (*)()> catalogue =
-        BuildViewCatalogue();
+        {
+            {"HostnameView", GetViewComponent},
+    };
     return catalogue;
 }
