a3997cf6683ed5a7f5cb806a94fef7df465bdcfd
David Given <dg@cowlark.com>
2026-09-26 16:32:20 +0200

    Add up/down buttons, although they're not working terribly well.

--- src/configuration.cc
+++ src/configuration.cc
@@ -55,19 +55,65 @@ void ConfigurationWindow::Draw(bool* open)
             DrawGlobalConfiguration();
         }
 
-        for (View* view : _views.GetAllViews())
+        std::vector<std::string> viewOrder =
+            GlobalPreferencesFetcher::GetViews(*_pendingPreferences);
         {
+            std::set<std::string> seen(viewOrder.begin(), viewOrder.end());
+            for (View* v : _views.GetAllViews())
+                if (!seen.contains(v->GetPrefName()))
+                    viewOrder.push_back(v->GetPrefName());
+        }
+
+        for (size_t i = 0; i < viewOrder.size(); ++i)
+        {
+            View* view = _views.Get(viewOrder[i]);
+            if (view == nullptr)
+            {
+                continue;
+            }
             ImGui::PushID(view->GetPrefName().c_str());
             std::string enabledKey = view->GetPrefName() + ".enabled";
             bool enabled =
                 _pendingPreferences->GetBoolean(enabledKey).value_or(true);
             bool isOpen = ImGui::CollapsingHeader(
                 view->GetHumanName().c_str(), ImGuiTreeNodeFlags_AllowOverlap);
-            ImGui::SameLine(ImGui::GetContentRegionMax().x -
-                            ImGui::GetFrameHeight() -
-                            ImGui::GetStyle().FramePadding.x);
+            float frameHeight = ImGui::GetFrameHeight();
+            float itemSpacing = ImGui::GetStyle().ItemSpacing.x;
+            float framePadding = ImGui::GetStyle().FramePadding.x;
+            float totalWidth = frameHeight * 3 + itemSpacing * 2;
+            ImGui::SameLine(
+                ImGui::GetContentRegionMax().x - totalWidth - framePadding);
+
+            bool upPressed = false;
+            bool downPressed = false;
+            if (i == 0)
+                ImGui::BeginDisabled();
+            upPressed = ImGui::Button((const char*) u8"↑", ImVec2(frameHeight, frameHeight));
+            if (i == 0)
+                ImGui::EndDisabled();
+            ImGui::SameLine(0, itemSpacing);
+            if (i + 1 >= viewOrder.size())
+                ImGui::BeginDisabled();
+            downPressed =
+                ImGui::Button((const char*) u8"↓", ImVec2(frameHeight, frameHeight));
+            if (i + 1 >= viewOrder.size())
+                ImGui::EndDisabled();
+            ImGui::SameLine(0, itemSpacing);
             if (ImGui::Checkbox("##enabled", &enabled))
                 _pendingPreferences->SetBoolean(enabledKey, enabled);
+            if (upPressed && i > 0)
+            {
+                std::vector<std::string> newOrder = viewOrder;
+                std::swap(newOrder[i], newOrder[i - 1]);
+                _pendingPreferences->SetStringList("views", newOrder);
+            }
+            if (downPressed && i + 1 < viewOrder.size())
+            {
+                std::vector<std::string> newOrder = viewOrder;
+                std::swap(newOrder[i], newOrder[i + 1]);
+                _pendingPreferences->SetStringList("views", newOrder);
+            }
+
             if (isOpen)
             {
                 view->DrawConfiguration(*_pendingPreferences);
